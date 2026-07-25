d038c62380fbd31475743183664d1122a852b1c8
shayan <shayan@akalptechnomediasolutions.com>
2026-07-25 11:17:41 +0530

    fix: import useRef in RoleManager.jsx

--- client/src/components/admin/RoleManager.jsx
+++ client/src/components/admin/RoleManager.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
+
 import { useDispatch } from "react-redux";
 import { loadCurrentUser } from "../../redux/slices/authSlice";
 import {
